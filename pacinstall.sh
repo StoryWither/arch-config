d404802c086af56e8adbeee8770585cf45cdf007
StoryWither <s.social@tutanota.com>
2021-10-30 21:47:47 +0200

    Add files via upload

---
+++ pacinstall.sh
@@ -0,0 +1,5 @@
+pacman -S alacritty discord dolphin firefox mpv mesa-vdpau neofetch p7zip --noconfirm
+pacman -S ttf-liberation ttf-sanzami ttf-ubuntu-font-family vlc wget xf86-video-amdgpu xf86-video-vesa--noconfirm
+pacman -R youtube-dl --noconfirm
+yay -S yt-dlp yt-dlp-drop-in ani-cli vibrant-cli
+pacman -S steam libva-mesa-driver htop vim --noconfirm
